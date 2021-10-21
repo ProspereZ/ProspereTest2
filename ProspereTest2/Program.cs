6b3ef14b7c3f9dc490dac97ee994526995a90caf
ProspereN <ProspereN@PC215-14>
2021-10-21 14:57:36 +0200

    Add project files.

---
+++ ProspereTest2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProspereTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studNaam;
+            int engelsPunt;
+            int proNetw;
+            int sum=0;
+           
+            Console.WriteLine("Enter the name");
+            studNaam = (Console.ReadLine());
+            Console.WriteLine("Enter the English value");
+            engelsPunt = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Programatie value");
+            proNetw = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine(studNaam + "Total Punt" + sum);
+                
+                sum = engelsPunt + proNetw;
+            }
+            while (engelsPunt >= 5 && engelsPunt <= 10);
+            while (proNetw >= 5 && proNetw <= 10);
+
+        }
+    }
+}
